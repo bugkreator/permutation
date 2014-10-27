39adc01765ac8c164f474f1af7b9cb1affef8202
unknown <orotem@7SSTWZ1.agoda.local>
2014-10-27 09:33:36 +0700

    toCycles, 1st iteration

--- src/main/scala/permutation.scala
+++ src/main/scala/permutation.scala
@@ -10,45 +10,66 @@ class Permutation(val func: (Int => Int))  extends Function1[Int, Int]
 		this ( Map(a->b, b->a) )
 	}
 
-
-	/*def this (Cycle: List[Int]) {
-		// single cycle in cycle notation
-		this(Cycle, if (Permutation.isValidCycle(Cycle)) true else throw new Exception ("Invalid Cycle"))
-	}*/
-
-
 	def this (Cycles: List[List[Int]]) {
-		this(Permutation.listsToCyclesMap(Cycles))
+		this({
+			val isCyclesValid: Boolean = if (Permutation.isValidCycleList(Cycles)) true else throw new Exception("Invalid Cycles List")
+			Permutation.listsToCyclesMap(Cycles)
+		})
 	}
 
 	private val fibers : Map[Int,List[Int]] = Permutation.allIntegers.groupBy(func)
 
 	private val assertValid: Boolean = {
 		// apply f to all integers. Make sure image lies inside allIntegers, and all fibers are singletons
-		val isValid : Boolean =  fibers.toList.forall(p => Permutation.isValidInteger(p._1) && (p._2.length==1))
+		val isValid : Boolean =  fibers.toList.forall({case (image, fiber) => Permutation.isValidInteger(image) && (fiber.length==1)})
 		if (isValid) true else (throw new Exception("Invalid Permutation"))
 	}
 
 	def apply(i: Int) : Int = {func(i)}
 	def Inverse: Permutation = new Permutation( (i:Int) => fibers(i).head )
 	def * (that: Permutation) : Permutation = new Permutation ( (i:Int) => this(that(i)) )
+
+	def toCycles: List[List[Int]] =
+	{
+		def helper(ValueSet: Set[Int], accCycles:List[List[Int]],  currCycleValues: List[Int]) : (Set[Int], List[List[Int]], List[Int]) =
+		{
+			if (ValueSet.isEmpty)
+				if (currCycleValues.isEmpty) (ValueSet, accCycles, currCycleValues)
+				else throw new Exception ("Something went wrong")
+			else
+			if (currCycleValues.isEmpty) helper(ValueSet.tail, accCycles, List(ValueSet.head))
+			else
+			{
+				val nextValue: Int = this.func(currCycleValues.last)
+				if (nextValue == currCycleValues.head) // reached full cycle. If it's not a 1-cycle, add it to the result & reset current cycle
+					helper(ValueSet - nextValue, if (currCycleValues.length == 1) accCycles else currCycleValues :: accCycles, Nil)
+				else
+					helper(ValueSet - nextValue, accCycles, currCycleValues :+ nextValue)
+			}
+		}
+
+		val (s,l, c) = helper(Permutation.allIntegers.toSet, Nil, Nil)
+		l
+	}
+
 }
 
 object Permutation {
 	val MAX_INT: Int = 20
 	def isValidInteger(i: Int) : Boolean = {i>=1 && i<=MAX_INT}
 	val allIntegers = (1 to MAX_INT).toList
-	val Identity : Permutation = new Permutation((i:Int) =>i)
+	val Identity : Permutation = new Permutation( (i:Int) => i)
+
 	private def hasNoDuplicates(l: List[Any]): Boolean = {
 		l.distinct.length == l.length
 	}
 
 	private def isValidCycleList(Cycles:List[List[Int]]) : Boolean =
 	{
-		hasNoDuplicates(Cycles.flatten) // no duplicates
+		hasNoDuplicates(Cycles.flatten)
 	}
 
-	private def listToCycleMap(StartingValue: Map[Int,Int], l:List[Int]):Map[Int,Int] = {
+	private def listToCycleMap(InitialMap: Map[Int,Int], l:List[Int]):Map[Int,Int] = {
 		def helper(current:List[Int], acc: Map[Int,Int]):Map[Int,Int] = {
 			current match {
 				case Nil => throw new Exception ("Empty. Did not expect to get here.")
@@ -57,7 +78,7 @@ object Permutation {
 			}
 		}
 		// wrap the list by adding the first element to the end
-		helper(l:+l.head, StartingValue)
+		helper(l:+l.head, InitialMap)
 	}
 
 	private def listsToCyclesMap(ls: List[List[Int]]) : Map[Int, Int] = {
--- src/main/scala/sheet1.sc
+++ src/main/scala/sheet1.sc
@@ -1,12 +1,8 @@
 object sheet1 {
 	val f: (Int => Int) = {(i:Int) => if (i==20) 1 else i+1}
-	def t: List[List[Int]] = List( List(1,2,5,3,11), List(2,4,6))
+	def t: List[List[Int]] = List( List(1,5,3,11), List(2,4,6))
 
-	t.flatten
-
-	//val p = new Permutation(t)
-
+	val p = Permutation.Identity
 	//Permutation.allIntegers.map(p)
-
-
+	p.toCycles
 }
